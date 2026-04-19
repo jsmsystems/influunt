2fc403b12bb0489129677410d76cf50cb6814c43
Marcio Cabanhas <mcabanhas@gmail.com>
2026-04-18 21:34:55 -0300

    save: salvando

--- docker/web-setup/setup-web.sh
+++ docker/web-setup/setup-web.sh
@@ -28,7 +28,7 @@ angular.module('environment', [])
 .constant('ENV', '${LOCAL_ENV_NAME}')
 .constant('PLACES_API', {"baseUrl":"http://egocet.prefeitura.sp.gov.br:8280/cet/geo"})
 .constant('MAP', {"url":"http://cetsp1.cetsp.com.br:10084/geoserver/cetmdc/wms?tiled=true","options":{"layers":["cetmdc:mdcViario_lg","cetmdc:mdcRotulos_lg"],"transparent":true,"format":"image/png8"}})
-.constant('MQTT_ROOT', {"url":"${LOCAL_MQTT_HOST}","port":${LOCAL_MQTT_PORT}})
+.constant('MQTT_ROOT', {"url": window.location.hostname || '${LOCAL_MQTT_HOST}' || 'localhost', "port":${LOCAL_MQTT_PORT}})
 .constant('ROOT_API_SMEE', 'http://appprod.cetsp.com.br/smee.webapicentraltempofixo/api');
 EOF
 
